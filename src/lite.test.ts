20990e5ac3ca1cc01d2e731efee00d8fba94c72f
flamrdevs <flamrdevs@gmail.com>
2022-09-30 14:45:36 +0700

    Hoam

--- src/lite.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-import { lite } from "./lite";
-
-describe("Lite", () => {
-  it("Equal", async () => {
-    expect(lite()).toEqual("lite");
-  });
-});
